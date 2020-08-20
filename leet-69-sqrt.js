23d5917d9ebb4bec4b2cec25f0209799d5bb6642
Balavijay <balavijay@gmail.com>
2020-08-20 16:21:40 +0530

    Absolute Sqrt

---
+++ leet-69-sqrt.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    return  parseInt(Math.sqrt(x)); 
+};
+
+console.log(mySqrt(9));
+console.log(mySqrt(8));
+console.log(mySqrt(19));
